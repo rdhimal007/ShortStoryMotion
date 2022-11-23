736f3364d1c3f1d7a34fcdff2c17c37823de8f2f
rdhimal007 <73763017+rdhimal007@users.noreply.github.com>
2022-11-22 23:37:45 -0600

    adding semi psudocode

---
+++ main.js
@@ -0,0 +1,41 @@
+// Name: Roshan Dhimal
+// Project: 
+
+
+// varaibles for the project cnavas
+let canvasWidth = 500;
+let canvasHeight = 400;
+let captionWidth = 500;
+let captionHeight = 100;
+let framteControl = 1;
+
+let imageArray = []; // populatte with image
+
+// load image
+function preload(){
+
+
+}
+
+// create class to design onject in the sketch
+class maketree{
+    constrructor(name, width, height){
+
+    }
+}
+
+// make button for the mouse handler
+function mousePressed(){
+
+}
+
+// sketch setup
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+
+
+}
+
+function draw() {
+    background(0);
+}
